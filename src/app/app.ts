f3f74d08f17a03afa401a023f5a76ca95da748f3
PynglTech <tech@pyngl.com>
2026-02-27 11:48:57 +0530

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -98,11 +98,9 @@ export class App implements OnInit {
   selectedSymbol = signal('RELIANCE');
 
   ngOnInit(): void {
-    // Load Pyngl loader script
     const script = document.createElement('script');
     script.src = '/loader.js';
     script.dataset['api'] = 'https://api.preta.io';
-    script.dataset['debug'] = 'true';
     document.head.appendChild(script);
   }
 
